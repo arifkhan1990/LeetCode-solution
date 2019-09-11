aa1450e6a80ef3a8e525df2ff7febc933bc30825
Arif Khan <arifkhanshubro@gmail.com>
2019-09-11 16:04:30 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- LeetCode-solution/problems/two_sum/solution.cpp
+++ LeetCode-solution/problems/two_sum/solution.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int> &numbers, int target) {
-        vector<int> result(2);
-        for (int i=0;i<numbers.size()-1;i++){
-            for (int j=i+1;j<numbers.size();j++){
-                if (numbers[i]+numbers[j]==target){
-                    result[0] = i,result[1] = j;
-                    return result;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>ans;
+        for(int i = 0; i < nums.size()-1 ; i++) {
+            for(int j = i+1; j < nums.size(); j++) {
+                if(nums[i]+nums[j] == target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
                 }
-            }    
+            }
         }
-        return result;
+        return ans;
     }
 };
