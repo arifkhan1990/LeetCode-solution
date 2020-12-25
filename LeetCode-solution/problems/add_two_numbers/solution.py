f54dd4fa84ded25c0c9a9ca62f215380132c8ea3
Arif Khan <arifkhanshubro@gmail.com>
2020-12-25 18:16:29 +0000

    Sync LeetCode submission - Add Two Numbers (python3)

--- LeetCode-solution/problems/add_two_numbers/solution.py
+++ LeetCode-solution/problems/add_two_numbers/solution.py
@@ -1,23 +1,37 @@
-class ListNode:
-    def __init__(self,x):
-        self.val = x
-        self.next = None
-        
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1, l2):
-        prev = result = ListNode(None)
-        carray = 0
-        
-        while l1 or l2 or carray:
-            if l1:
-                carray += l1.val
-                l1 = l1.next
-            if l2:
-                carray += l2.val
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        c = i = 0
+        new_node = prev = None
+
+        while l1 or l2:
+            if l1 is None:
+                data = (l2.val + c)%10
+                c = (l2.val + c)//10
                 l2 = l2.next
+                
+            elif l2 is None:
+                data = (l1.val + c)%10
+                c = (l1.val + c)//10
+                l1 = l1.next
+            else:
+                data = (l1.val + l2.val + c)%10
+                c = (l1.val + l2.val + c)//10
+                l1 = l1.next
+                l2 = l2.next
+            new_node = ListNode(data)
+            if i == 0:
+                head = new_node
+            else:
+                prev.next = new_node
+
+            prev = new_node
+            if c:
+                new_node.next = ListNode(c)
+            i += 1
+        return head
             
-            prev.next = ListNode(carray % 10)
-            prev = prev.next
-            carray //= 10
-        
-        return result.next
