b79d3d99c7de2662df3043f1454876413bb1d90e
Arif Khan <arifkhanshubro@gmail.com>
2022-08-18 10:14:24 +0000

    Sync LeetCode submission - Decode XORed Array (python3)

---
+++ LeetCode-solution/problems/decode_xored_array/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        ans = [first]
+        for i in encoded:
+            ans.append(first^i)
+            first = first^i
+        return ans
