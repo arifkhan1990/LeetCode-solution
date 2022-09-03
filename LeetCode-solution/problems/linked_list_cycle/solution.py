899ea93c32fbae21fc3fb62010cddde548ecbee2
Arif Khan <arifkhanshubro@gmail.com>
2022-09-03 18:51:12 +0000

    Sync LeetCode submission - Linked List Cycle (python3)

--- LeetCode-solution/problems/linked_list_cycle/solution.py
+++ LeetCode-solution/problems/linked_list_cycle/solution.py
@@ -5,14 +5,16 @@
 #         self.next = None
 
 class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
         if head is None:
             return False
+        p = q = head
         
-        fast = slow = head
-        while fast is not None and fast.next is not None:
-            fast, slow = fast.next.next, slow.next
-            
-            if fast == slow:
+        while q is not None and q.next is not None:
+            p = p.next
+            q = q.next.next
+            if p == q:
                 return True
         return False
+        
+            
