33b435bb4256a77b2de2d1491ab4e6bda5a153c2
Arif Khan <arifkhanshubro@gmail.com>
2022-09-05 23:37:39 +0600

    Time: 468 ms (5.62%), Space: 18.5 MB (21.66%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -1,4 +1,10 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        s[:] = s[::-1]
+        #  two pointer solution
+        f , l = 0, len(s)-1
+        while f <= l:
+            s[f], s[l] = s[l], s[f]
+            f , l = f+1, l-1
+            
+        # s[:] = s[::-1] python single line solution
         
