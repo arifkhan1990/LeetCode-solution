400cbfebb96601b8d8d3024f8b4a8801b0b63681
Arif Khan <arifkhanshubro@gmail.com>
2022-10-01 06:56:29 +0000

    Sync LeetCode submission - Count Number of Pairs With Absolute Difference K (python3)

---
+++ LeetCode-solution/problems/count_number_of_pairs_with_absolute_difference_k/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        ans = 0
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i != j:
+                    if nums[j] - nums[i] == k:
+                        ans += 1
+        return ans
