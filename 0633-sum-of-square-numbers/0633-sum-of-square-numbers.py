cfeedfe6c179694b858edf3a93b200bb75af1c03
Arif Khan <arifkhanshubro@gmail.com>
2022-10-29 22:32:47 +0600

    Time: 90 ms (98.80%), Space: 13.8 MB (63.09%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,7 +1,30 @@
 import math
 class Solution:
+    # def binarySearch(self, l, h , t):
+    #     if l > e:
+    #         return 0
+    #     m = l + (h - l)//2
+    #     # print(m , m*m, t)
+    #     if (m*m) == t:
+    #         return 1
+    #     if (m*m) > t:
+    #         return self.binarySearch(l, m-1, t)
+    #     return self.binarySearch(m+1, h , t)
     
     def judgeSquareSum(self, c: int) -> bool:
+        # Fermat Theorem
+        i = 2
+        while i*i <= c:
+            co = 0
+            if c%i == 0:
+                while c%i == 0:
+                    co += 1
+                    c /= i
+                if i%4 == 3 and co % 2 != 0:
+                    return 0
+            i += 1
+        return c%4 != 3
+            
         # i = 0
         # while i*i <= c:   # TLE
         #     b = c - i*i
@@ -14,10 +37,20 @@ class Solution:
         #     i += 1
         # return 0
         
-        i = 0
-        while i*i <= c: 
-            b = math.sqrt(c - i*i)
-            if b == int(b):
-                return 1
-            i += 1
-        return 0
+        # sqrt approch
+        # i = 0
+        # while i*i <= c: 
+        #     b = math.sqrt(c - i*i)
+        #     if b == int(b):
+        #         return 1
+        #     i += 1
+        # return 0
+       # binary search and sqrt
+        # i = 0
+        # while i*i <= c:
+        #     b = c - (i*i)
+        #     if self.binarySearch(0,b , b):
+        #         return 1
+        #     i += 1
+        # return 0
+        
