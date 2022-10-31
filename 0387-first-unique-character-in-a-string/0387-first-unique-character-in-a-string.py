a7f55567928e8254004fec964753e498070b4cf9
Arif Khan <arifkhanshubro@gmail.com>
2022-10-31 12:34:11 +0600

    Time: 205 ms (62.90%), Space: 13.9 MB (99.71%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        h = Counter(s)
+        
+        for i in range(len(s)):
+            if h[s[i]] == 1:
+                return i
+
+        return -1
