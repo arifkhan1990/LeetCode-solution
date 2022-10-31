1c5ecd3a2c63b76b3d91d29dfdd65f12a6d61a96
Arif Khan <arifkhanshubro@gmail.com>
2022-10-31 14:00:23 +0600

    Time: 120 ms (44.40%), Space: 14.2 MB (53.60%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -2,7 +2,8 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         r = collections.Counter(ransomNote)
         m = collections.Counter(magazine)
-        for i in r:
-            if i not in m or r[i] > m[i]:
-                return 0
-        return 1
+        # for i in r:
+        #     if i not in m or r[i] > m[i]:
+        #         return 0
+        # return 1
+        return not r - m
