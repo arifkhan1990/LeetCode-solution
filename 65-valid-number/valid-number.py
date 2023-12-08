927f18872d3301ddecb00a73fceec2b0756e6d12
Arif Khan <arifkhanshubro@gmail.com>
2023-12-09 02:31:01 +0600

    Time: 37 ms (84.04%) | Memory: 16.4 MB (25.91%) - LeetSync

--- 65-valid-number/valid-number.py
+++ 65-valid-number/valid-number.py
@@ -1,7 +1,33 @@
 class Solution:
     def isNumber(self, s: str) -> bool:
-        # Define the regular expression pattern for a valid number
-        pattern = re.compile(r'^[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?$')
-        
-        # Check if the string matches the pattern
-        return bool(pattern.match(s.strip()))
+        states = [
+            {'digit': 1, 'sign': 2, 'dot': 3},
+            {'digit': 1, 'dot': 4, 'exp': 5},
+            {'digit': 1, 'dot': 3},
+            {'digit': 4},
+            {'digit': 4, 'exp': 5},
+            {'sign': 6, 'digit': 7},
+            {'digit': 7},
+            {'digit': 7}
+        ]
+
+        current_state = 0
+
+        for char in s:
+            if char.isdigit():
+                key = 'digit'
+            elif char == '.':
+                key = 'dot'
+            elif char in {'+', '-'}:
+                key = 'sign'
+            elif char in {'e', 'E'}:
+                key = 'exp'
+            else:
+                return False
+
+            if key not in states[current_state]:
+                return False
+
+            current_state = states[current_state][key]
+
+        return current_state in {1, 4, 7}
