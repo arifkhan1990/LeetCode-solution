6a527b3585181cc956ebdf32110bc5c03c8fa9bd
Arif Khan <arifkhanshubro@gmail.com>
2023-12-11 12:33:35 +0600

    Time: 384 ms (13.87%) | Memory: 22.4 MB (45.16%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
@@ -1,5 +1,11 @@
 class Solution:
     def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
-        sorted_score = sorted(score, key=lambda x: x[k], reverse=True)
-        return sorted_score
+        n = len(score)
+        for i in range(n):
+            max_index = i
+            for j in range(i + 1, n):
+                if score[j][k] > score[max_index][k]:
+                    max_index = j
+            score[i], score[max_index] = score[max_index], score[i]
+        return score
         
