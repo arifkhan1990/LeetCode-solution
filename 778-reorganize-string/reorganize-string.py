8e9ad04f785b60672d2b18cec22df3eb305921a5
Arif Khan <arifkhanshubro@gmail.com>
2024-01-15 21:36:38 +0600

    Time: 37 ms (77.06%) | Memory: 17.4 MB (23.42%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,32 +1,36 @@
 from collections import Counter
+from heapq import heapify, heappop, heappush
+from itertools import zip_longest
+
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        cnt = Counter(s)
-        n = len(s)
-        mx_data = cnt.most_common()[0]
-        mx_ch, mx_v = mx_data[0], mx_data[1]
-
-        if mx_v * 2 > n+1:
-            return ""
+        ch_cnt = {}
+        for ch in s:
+            ch_cnt[ch] = ch_cnt.get(ch, 0) + 1
         
-        longest = []
-        smallest = []
-        longest.extend(mx_ch*mx_v)
+        max_heap = [(-cnt, ch) for ch , cnt in ch_cnt.items()]
+        heapq.heapify(max_heap)
 
-        for k in cnt.keys():
-            if k == mx_ch:
-                continue
+        res = []
+
+        while len(max_heap) >= 2:
+            cnt1, ch1 = heapq.heappop(max_heap)
+            cnt2, ch2 = heapq.heappop(max_heap)
+
+            res.extend([ch1, ch2])
+
+            if cnt1 + 1 < 0:
+                heapq.heappush(max_heap, (cnt1 + 1, ch1))
+            if cnt2 + 1 < 0:
+                heapq.heappush(max_heap, (cnt2 + 1, ch2))
+        
+        if max_heap:
+            cnt, ch = heapq.heappop(max_heap)
+            res.append(ch)
+
+            if cnt + 1 < 0:
+                return ""
+        return "".join(res)
             
-            for _ in range(cnt[k]):
-                if (len(longest) + 1) * 2 <= n+1:
-                    longest.append(k)
-                else:
-                    smallest.append(k)
 
-        ans = ""
-        for a, b in zip_longest(longest, smallest):
-            ans += a
-            if b is not None:
-                ans += b
-        
-        return ans
+
