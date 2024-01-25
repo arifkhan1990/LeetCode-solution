395f4bcbb65cdd905112ae1dc7ccf08d945ff96b
Arif Khan <arifkhanshubro@gmail.com>
2024-01-25 22:23:59 +0600

    Time: 45 ms (96.00%) | Memory: 16.7 MB (73.12%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -3,15 +3,19 @@ class Solution:
         ans = []
         def backtrack(start):
             if start == len(nums) - 1:
-                if nums not in ans:
-                    ans.append(nums.copy())
+                ans.append(nums.copy())
                 return
-            
+            used = set()
+
             for i in range(start, len(nums)):
+                if nums[i] in used:
+                    continue
                 nums[start], nums[i] = nums[i], nums[start]
 
                 backtrack(start+1)
 
                 nums[start], nums[i] = nums[i], nums[start]
+
+                used.add(nums[i])
         backtrack(0)
         return ans
