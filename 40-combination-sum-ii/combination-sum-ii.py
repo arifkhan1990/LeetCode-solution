634bae23e3efd07bd98224a96639b3c4c8c7a956
Arif Khan <arifkhanshubro@gmail.com>
2024-01-28 08:05:42 +0600

    Time: 51 ms (85.45%) | Memory: 16.7 MB (60.23%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,6 +1,8 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        def backtrack(s, t, p):
+        ans = []
+
+        def solve(s, t, p):
             if t == 0:
                 ans.append(p[:])
                 return
@@ -8,14 +10,12 @@ class Solution:
             for i in range(s, len(candidates)):
                 if i > s and candidates[i] == candidates[i-1]:
                     continue
-
                 if candidates[i] > t:
                     continue
 
                 p.append(candidates[i])
-                backtrack(i+1, t-candidates[i], p)
+                solve(i+1, t-candidates[i], p)
                 p.pop()
-        ans = []
         candidates.sort()
-        backtrack(0, target, [])
+        solve(0, target, [])
         return ans
