db1d3afaa04bee1a9a212b0b79322166a17be8e6
Arif Khan <arifkhanshubro@gmail.com>
2024-01-29 23:02:42 +0600

    Time: 35 ms (83.31%) | Memory: 16.8 MB (63.73%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,24 +1,14 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        ans = []
+        res = []
+        def dfs(curr,toOpen,opened,res):
+            if toOpen == 0 and opened == 0:
+                res.append(curr)
+                return
+            if toOpen > 0:
+                dfs(curr+ "(",toOpen-1,opened + 1,res)
+            if opened > 0:
+                dfs(curr + ")",toOpen, opened-1,res)
 
-        def solve(path="", char="(", open=0, close=0):
-            if len(path) == n*2:
-                ans.append(path)
-            else:
-                for ch in char:
-                    cur_path = path+ch
-                    if ch == '(':
-                        c_open = open+1
-                        c_close = close
-                    else:
-                        c_open = open
-                        c_close = close + 1
-                    new_ch = ""
-                    if c_open < n:
-                        new_ch += "("
-                    if c_close < c_open:
-                        new_ch += ")"
-                    solve(cur_path, new_ch, c_open, c_close)
-        solve()
-        return ans
+        dfs("",n,0,res)
+        return res
