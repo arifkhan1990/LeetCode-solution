66e74fe87beeb3e17466e8d17a2650d8e3c4625b
Arif Khan <arifkhanshubro@gmail.com>
2024-03-06 18:04:18 +0600

    Time: 1149 ms (5.02%) | Memory: 18.8 MB (64.05%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.py
+++ 740-delete-and-earn/delete-and-earn.py
@@ -1,15 +1,16 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         n = len(nums)
-        sm = [0]*(10001)
+        mx = max(nums)+1
+        sm = [0]*(mx)
 
         for i in set(nums):
             sm[i] = nums.count(i) * i
         
-        dp = [0] * (10001)
+        dp = [0] * (mx)
         dp[1] = sm[1]
         
-        for i in range(2,10001):
+        for i in range(2,mx):
             dp[i] = max(dp[i-2] + sm[i], dp[i-1])
         
-        return dp[10000]
+        return dp[mx-1]
