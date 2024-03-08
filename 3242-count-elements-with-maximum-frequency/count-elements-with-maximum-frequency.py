38025663cdceffc945a48dffc09def7ffde58b6a
Arif Khan <arifkhanshubro@gmail.com>
2024-03-08 06:59:36 +0600

    Time: 38 ms (87.93%) | Memory: 16.5 MB (97.35%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
@@ -2,12 +2,6 @@ from collections import Counter
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
         freq = Counter(nums)
-        cnt = list(freq.values())
-        cnt.sort()
-        ans = []
-        for i in range(len(cnt) - 1, -1, -1):
-            if len(ans) > 0 and cnt[i] != ans[-1]:
-                break
-            else:
-                ans.append(cnt[i])
-        return sum(ans)
+        max_freq = max(freq.values())
+        ans = sum(val for val in freq.values() if val == max_freq)
+        return ans
