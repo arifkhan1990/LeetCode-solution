13351a1704912bd0a595690eb6455b87fb6e958a
Arif Khan <arifkhanshubro@gmail.com>
2024-03-09 20:48:05 +0600

    Time: 341 ms (84.08%) | Memory: 39.7 MB (36.50%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,11 +1,6 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        ans = {}
-        res = 0
-        for i in nums1:
-            if i not in ans:
-                ans[i] = 1
-        
+        ans = set(nums1)
         for i in nums2:
             if i in ans:
                 return i
