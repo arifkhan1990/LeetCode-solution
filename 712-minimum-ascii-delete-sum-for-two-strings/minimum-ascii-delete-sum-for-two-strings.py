827a217407c0400bf7d56bdc925fd3024d088d78
Arif Khan <arifkhanshubro@gmail.com>
2024-03-10 18:29:51 +0600

    Time: 92 ms (99.85%) | Memory: 16.6 MB (99.25%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -1,26 +1,12 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        memo = {}
-
-        def solve(i, j, curr):
-            if (i, j) in memo:
-                return memo[(i, j)]
-
-            if i == len(s1) and j == len(s2):
-                result = 0
-            else:
-                result = float('inf')
-
-            if i < len(s1) and j < len(s2) and s1[i] == s2[j]:
-                result = solve(i + 1, j + 1, curr)
-            else:
-                if i < len(s1):
-                    result = min(result, ord(s1[i]) + solve(i + 1, j, curr))
-                
-                if j < len(s2):
-                    result = min(result, ord(s2[j]) + solve(i, j + 1, curr))
-
-            memo[(i, j)] = result
-            return result
-
-        return solve(0, 0, 0)
+        dp = [0] * len(s2)
+        for c1 in s1:
+            prev = 0
+            for i, c2 in enumerate(s2):
+                last = dp[i]
+                if c1 == c2:
+                    dp[i] = prev + ord(c1)
+                if prev < last:
+                    prev = last
+        return sum(map(ord, f'{s1}{s2}')) - 2 * max(dp)
