399cd078867f4349e653c6a631a3c5b8e340c319
Arif Khan <arifkhanshubro@gmail.com>
2024-03-10 08:17:07 +0600

    Time: 104 ms (55.12%) | Memory: 18.7 MB (80.63%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,19 +1,24 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        @cache
-        def solve(w1, w2, i, j) :
+        def solve(w1, w2, i, j, dp):
             if i == len(w1):
                 return len(w2) - j
             if j == len(w2):
                 return len(w1) - i
             
-            ans = 0
+            if dp[i][j] != -1:
+                return dp[i][j]
+            
             if w1[i] == w2[j]:
-                return solve(w1,w2, i+1, j+1)
+                return solve(w1, w2, i+1, j+1, dp)
             else:
-                insert = 1 + solve(w1,w2, i, j+1)
-                delete = 1 + solve(w1,w2, i+1, j)
-                replace = 1 + solve(w1,w2, i+1, j+1)
-                ans = min([insert, delete, replace])
-            return ans
-        return solve(word1, word2, 0, 0)
+                insert = 1 + solve(w1, w2, i, j+1, dp)
+                delete = 1 + solve(w1, w2, i+1, j, dp)
+                replace = 1 + solve(w1, w2, i+1, j+1, dp)
+                ans = min(insert, delete, replace)
+            
+            dp[i][j] = ans
+            return dp[i][j]
+        
+        dp = [[-1] * (len(word2)) for _ in range(len(word1))]
+        return solve(word1, word2, 0, 0, dp)
