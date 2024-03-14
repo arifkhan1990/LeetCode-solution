7c75963269386b6196e153657e7b91965af11dbc
Arif Khan <arifkhanshubro@gmail.com>
2024-03-14 21:21:57 +0600

    Time: 42 ms (87.02%) | Memory: 17.3 MB (78.06%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
@@ -6,12 +6,13 @@
 #         self.right = right
 class Solution:
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        dp = {}
         def solve(l, r):
-            if l == r:
-                return [TreeNode(l)]
             if l > r:
                 return [None]
             
+            if (l, r) in dp:
+                return dp[(l,r)]
             ans = []
 
             for i in range(l, r+1):
@@ -19,5 +20,6 @@ class Solution:
                     for rn in solve(i+1, r):
                         root = TreeNode(i, ln, rn)
                         ans.append(root)
+            dp[(l,r)] = ans
             return ans
         return solve(1, n)
