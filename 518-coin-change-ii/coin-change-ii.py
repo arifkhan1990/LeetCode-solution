6c22adc3afa0f2a8f5a32aafb2ffd3a1984b204c
Arif Khan <arifkhanshubro@gmail.com>
2024-03-15 08:30:50 +0600

    Time: 87 ms (94.31%) | Memory: 25.7 MB (59.92%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,13 +1,21 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        @cache
-        def solve(idx, t):
+        def solve(idx, t, dp):
             if idx == 0:
-                return 0 if t%coins[0] != 0 else 1
-            ntake = solve(idx-1, t)
+                if t%coins[0] != 0:
+                    dp[idx][t] = 0
+                else:
+                    dp[idx][t] = 1
+                return dp[idx][t]
+            if dp[idx][t] != -1:
+                return dp[idx][t]
+            ntake = solve(idx-1, t, dp)
             take = 0
             if coins[idx] <= t:
-                take = solve(idx, t-coins[idx])
+                take = solve(idx, t-coins[idx], dp)
+            dp[idx][t] = take+ntake
+            return dp[idx][t]
 
-            return take + ntake
-        return solve(len(coins)-1, amount)
+        n = len(coins)
+        dp = [[-1]*(amount+1) for _ in range(n+1)]
+        return solve(n-1, amount, dp)
