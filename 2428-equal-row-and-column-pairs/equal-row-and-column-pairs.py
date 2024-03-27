da7452936c996f19fb9f5a8b999cfb6ceac86b38
Arif Khan <arifkhanshubro@gmail.com>
2024-03-27 06:47:06 +0600

    Time: 364 ms (95.73%) | Memory: 21.4 MB (52.90%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -1,16 +1,27 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        matrix = grid.copy()
-        transposed_matrix = []
-        for i in range(len(matrix[0])):
-            transposed_row = []
-            for row in matrix:
-                transposed_row.append(row[i])
-            transposed_matrix.append(transposed_row)
-        
         ans = 0
-        for i in range(len(matrix[0])):
-            for j in range(len(matrix[0])):
-                if grid[i] == transposed_matrix[j]:
-                    ans += 1
+        convert = defaultdict(int)
+
+        for row in grid:
+            convert[tuple(row)] += 1
+        
+        for col in zip(*grid):
+            ans += convert[tuple(col)]
         return ans
+        # this is another solution
+
+        # matrix = grid.copy()
+        # transposed_matrix = []
+        # for i in range(len(matrix[0])):
+        #     transposed_row = []
+        #     for row in matrix:
+        #         transposed_row.append(row[i])
+        #     transposed_matrix.append(transposed_row)
+        
+        # ans = 0
+        # for i in range(len(matrix[0])):
+        #     for j in range(len(matrix[0])):
+        #         if grid[i] == transposed_matrix[j]:
+        #             ans += 1
+        # return ans
