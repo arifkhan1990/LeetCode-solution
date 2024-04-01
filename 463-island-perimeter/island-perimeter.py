d8e23602f021bf8ba38b4fb4fa4ed48d5b9e3919
Arif Khan <arifkhanshubro@gmail.com>
2024-04-01 07:46:18 +0600

    Time: 341 ms (94.29%) | Memory: 16.9 MB (92.36%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.py
@@ -0,0 +1,13 @@
+class Solution:
+    def islandPerimeter(self, mat: List[List[int]]) -> int:
+
+        ans = 0
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if mat[i][j] == 1:
+                    ans += 4
+                    if i > 0 and mat[i-1][j] == 1:  # Check top neighbor
+                        ans -= 2
+                    if j > 0 and mat[i][j-1] == 1:  # Check left neighbor
+                        ans -= 2
+        return ans 
