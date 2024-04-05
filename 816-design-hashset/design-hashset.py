fa9f910eeacf48e602e62fb125835eb2b2784b8e
Arif Khan <arifkhanshubro@gmail.com>
2024-04-05 20:33:58 +0600

    Time: 115 ms (87.79%) | Memory: 21.3 MB (71.32%) - LeetSync

--- 816-design-hashset/design-hashset.py
+++ 816-design-hashset/design-hashset.py
@@ -1,17 +1,25 @@
 class MyHashSet:
 
     def __init__(self):
-        self.hash_set = set()
+        self.size = 1000
+        self.data = [[] for _ in range(self.size)]
+
+    def _hash(self, key):
+        return key % self.size
 
     def add(self, key: int) -> None:
-        self.hash_set.add(key)
+        idx = self._hash(key)
+        if key not in self.data[idx]:
+            self.data[idx].append(key)
 
     def remove(self, key: int) -> None:
-        if key in self.hash_set:
-            self.hash_set.remove(key)
+        idx = self._hash(key)
+        if key in self.data[idx]:
+            self.data[idx].remove(key)
 
     def contains(self, key: int) -> bool:
-        return key in self.hash_set
+        idx = self._hash(key)
+        return key in self.data[idx]
 
 
 # Your MyHashSet object will be instantiated and called as such:
