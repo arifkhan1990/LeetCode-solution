9dc53494e65d802db2c9f450d343b0d4e8ca920d
Arif Khan <arifkhanshubro@gmail.com>
2024-04-10 03:50:55 +0600

    Time: 969 ms (56.90%) | Memory: 32.3 MB (78.71%) - LeetSync

--- 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.py
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.py
@@ -1,16 +1,15 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         nums.sort()
+        l, r = 0, len(nums)-1
+
         pro = [0]*(len(nums))
-        n = len(nums)//2
-        n1, n2 = nums[:-n], nums[-n:]
         
         for i in range(len(nums)):
             if i%2 == 0:
-                pro[i] = n1[0]
-                n1.pop(0)
-        for i in range(1,len(nums)):
-            if i%2 != 0:
-                pro[i] = n2[0]
-                n2.pop(0) 
+                pro[i] = nums[l]
+                l += 1
+            else:
+                pro[i] = nums[r]
+                r -= 1
         return pro
