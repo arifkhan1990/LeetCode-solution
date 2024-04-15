eac3747607a10f49f86e1a0fd066bc875c718595
Arif Khan <arifkhanshubro@gmail.com>
2024-04-15 20:11:18 +0600

    Time: 93 ms (37.62%) | Memory: 16.9 MB (8.68%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,25 +1,24 @@
-from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         l1, l2 = len(s1), len(s2)
-        if l1 > l2 :
-            return 0
 
+        if l1 > l2:
+            return 0
         
         cnt = Counter(s1)
-        curr_window = Counter(s2[:l1])
+        cur_wndw = Counter(s2[:l1])
 
-        if curr_window  == cnt:
+        if cur_wndw == cnt:
             return 1
         
         for i in range(l1, l2):
-            curr_window[s2[i]] += 1
+            cur_wndw[s2[i]] += 1
 
-            if curr_window[s2[i - l1]] == 1:
-                del curr_window[s2[i-l1]]
+            if cur_wndw[s2[i - l1]] == 1: 
+                del cur_wndw[s2[i-l1]]
             else:
-                curr_window[s2[i-l1]] -= 1
+                cur_wndw[s2[i-l1]] -= 1
             
-            if curr_window == cnt:
+            if cur_wndw == cnt:
                 return 1
         return 0
