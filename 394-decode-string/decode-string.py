8f8b22611203e177a1eb1f058ce83d738e64fcb9
Arif Khan <arifkhanshubro@gmail.com>
2024-04-26 04:44:49 +0600

    Time: 34 ms (60.96%) | Memory: 16.5 MB (92.19%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,16 +1,16 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stc, cn , cst = [], 0 , ''
+        st , curr_n , curr_s = [], 0, ''
 
         for ch in s:
             if ch.isdigit():
-                cn = cn * 10 + int(ch)
+                curr_n = curr_n * 10 + int(ch)
             elif ch == '[':
-                stc.append((cst, cn))
-                cst, cn = '', 0
+                st.append((curr_s, curr_n))
+                curr_s, curr_n = '', 0
             elif ch == ']':
-                pst, n = stc.pop()
-                cst = pst + cst * n
+                pre_s, num = st.pop()
+                curr_s = pre_s + curr_s * num
             else:
-                cst += ch
-        return cst
+                curr_s += ch
+        return curr_s
